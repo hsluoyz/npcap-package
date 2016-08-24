888e7f6fc2ba985f08bc93ec02f98e129763d589
Yang Luo <hsluoyz@qq.com>
2016-08-24 16:18:40 +0800

    Changed "-packageParameters" to "--install-arguments".

--- tools/ChocolateyInstall.ps1
+++ tools/ChocolateyInstall.ps1
@@ -1,14 +1,5 @@
 ﻿$name   = 'npcap'
-$url = 'https://github.com/nmap/npcap/releases/download/v0.08-r4/npcap-0.08-r4.exe'
+$url = 'https://github.com/nmap/npcap/releases/download/v0.08-r7/npcap-0.08-r7.exe'
 $silent = '/S'
-
-# Now we can use the $env:chocolateyPackageParameters inside the Chocolatey package
-$packageParameters = $env:chocolateyPackageParameters
-
-if ($packageParameters) {
-	$silent = $silent + ' ' + $packageParameters
-}
-
-Write-Debug "Silent arguments for the installer: $silent"
 
 Install-ChocolateyPackage $name 'exe' $silent $url

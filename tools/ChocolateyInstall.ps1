6ba2cb9cf0abdeb7bb09369c70b1beca2bb18502
Yang Luo <hsluoyz@qq.com>
2016-12-21 09:42:42 +0800

    Added checksum to the install script.

--- tools/ChocolateyInstall.ps1
+++ tools/ChocolateyInstall.ps1
@@ -2,4 +2,4 @@
 $url = 'https://github.com/nmap/npcap/releases/download/v0.78/npcap-0.78.exe'
 $silent = '/S'
 
-Install-ChocolateyPackage $name 'exe' $silent $url
+Install-ChocolateyPackage $name 'exe' $silent $url -checksum '0D03DA9029C22C1E80A2420C2BD0F6F388FB7CFA4518168D8C41F6D8D002ECDC' -checksumType 'sha256'

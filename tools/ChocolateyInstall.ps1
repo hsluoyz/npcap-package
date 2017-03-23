7aab9a38510f5ab4881622cf73bdfddcc0db5d52
Rui Lopes <rgl@ruilopes.com>
2017-03-23 08:51:32 +0000

    Updated to 0.83.

--- tools/ChocolateyInstall.ps1
+++ tools/ChocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name   = 'npcap'
-$url = 'https://nmap.org/npcap/dist/npcap-0.82.exe'
+$url = 'https://nmap.org/npcap/dist/npcap-0.83.exe'
 $silent = '/S'
 
-Install-ChocolateyPackage $name 'exe' $silent $url -checksum '6401EC91B9A09D3295D329CA01A68872F71E749842BA1AF8D35DD933F2840EF5' -checksumType 'sha256'
+Install-ChocolateyPackage $name 'exe' $silent $url -checksum 'ad7ec0eea55e2dfb062580decde032a2584c106b2e4fd84782e81304a380d64a' -checksumType 'sha256'
